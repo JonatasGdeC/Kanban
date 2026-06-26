947494e4e048d811f33d6fc0a20a5eb21f8672a9
JonatasGdeC <jonatas.07022003@gmail.com>
2026-06-26 14:21:11 -0300

    Criando 'UserValidatorTests'

--- src/Backend/Kanban.Application/UseCase/User/Update/UpdateUserValidator.cs
+++ src/Backend/Kanban.Application/UseCase/User/Update/UpdateUserValidator.cs
@@ -9,11 +9,13 @@ public class UpdateUserValidator : AbstractValidator<UpdateUserRequest>
     public UpdateUserValidator()
     {
         RuleFor(expression: request => request.Name)
+            .Cascade(cascadeMode: CascadeMode.Stop)
             .NotEmpty().WithMessage(errorMessage: ResourceErrorMessage.NAME_IS_REQUIRED)
             .MinimumLength(minimumLength: 3).WithMessage(errorMessage: ResourceErrorMessage.NAME_MINIMUM_LENGTH)
             .MaximumLength(maximumLength: 200).WithMessage(errorMessage: ResourceErrorMessage.NAME_MAXIMUM_LENGTH);
 
         RuleFor(expression: request => request.Email)
+            .Cascade(cascadeMode: CascadeMode.Stop)
             .NotEmpty().WithMessage(errorMessage: ResourceErrorMessage.EMAIL_IS_REQUIRED)
             .EmailAddress().WithMessage(errorMessage: ResourceErrorMessage.EMAIL_INVALID);
     }
---
+++ tests/Backend/Validators.Tests/User/UserValidatorTests.cs
@@ -0,0 +1,91 @@
+using CommomTestsUtilies.Requests;
+using FluentAssertions;
+using FluentValidation.Results;
+using Kanban.Application.UseCase.User;
+using Kanban.Communication.Requests.User;
+using Kanban.Exception;
+
+namespace Validators.Tests.User;
+
+public class UserValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        ValidationResult? validator = new UserValidator().Validate(instance: RegisterUserRequestBuilder.Build());
+        validator.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData(data: null)]
+    public void Error_NameIsRequired(string name)
+    {
+        RegisterUserRequest request = RegisterUserRequestBuilder.Build();
+        request.Name = name;
+
+        ValidationResult? validator = new UserValidator().Validate(instance: request);
+
+        validator.IsValid.Should().BeFalse();
+        validator.Errors.Should().ContainSingle().And
+            .Contain(predicate: e => e.ErrorMessage.Equals(ResourceErrorMessage.NAME_IS_REQUIRED));
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(201)]
+    public void Error_NameLengthInvalid(int length)
+    {
+        RegisterUserRequest request = RegisterUserRequestBuilder.Build();
+        request.Name = new string(c: 'a', count: length);
+
+        ValidationResult? validator = new UserValidator().Validate(instance: request);
+
+        validator.IsValid.Should().BeFalse();
+
+        if (length <= 2)
+        {
+            validator.Errors.Should().ContainSingle().And
+                .Contain(predicate: e => e.ErrorMessage.Equals(ResourceErrorMessage.NAME_MINIMUM_LENGTH));
+        }
+
+        if (length >= 201)
+        {
+            validator.Errors.Should().ContainSingle().And
+                .Contain(predicate: e => e.ErrorMessage.Equals(ResourceErrorMessage.NAME_MAXIMUM_LENGTH));
+        }
+    }
+    
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData(data: null)]
+    public void Error_EmailIsRequired(string email)
+    {
+        RegisterUserRequest request = RegisterUserRequestBuilder.Build();
+        request.Email = email;
+
+        ValidationResult? validator = new UserValidator().Validate(instance: request);
+
+        validator.IsValid.Should().BeFalse();
+        validator.Errors.Should().ContainSingle().And
+            .Contain(predicate: e => e.ErrorMessage.Equals(ResourceErrorMessage.EMAIL_IS_REQUIRED));
+    }
+    
+    
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        RegisterUserRequest request = RegisterUserRequestBuilder.Build();
+        string newEmail = request.Email.Replace(oldChar: '@', newChar: '.');
+        request.Email = newEmail;
+
+        ValidationResult? result = new UserValidator().Validate(instance: request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(predicate: e => e.ErrorMessage.Equals(ResourceErrorMessage.EMAIL_INVALID));
+    }
+}
