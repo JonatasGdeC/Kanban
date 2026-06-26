1825a146d7e7ee5c6227f9107edccabd6c48fc1c
JonatasGdeC <jonatas.07022003@gmail.com>
2026-06-26 15:15:04 -0300

    Criando teste para SubTaskValidator

--- src/Backend/Kanban.Application/UseCase/SubTask/SubTaskValidator.cs
+++ src/Backend/Kanban.Application/UseCase/SubTask/SubTaskValidator.cs
@@ -9,6 +9,7 @@ public class SubTaskValidator : AbstractValidator<RegisterSubTaskRequest>
     public SubTaskValidator()
     {
         RuleFor(expression: request => request.Name)
+            .Cascade(cascadeMode: CascadeMode.Stop)
             .NotEmpty().WithMessage(errorMessage: ResourceErrorMessage.NAME_IS_REQUIRED)
             .MinimumLength(minimumLength: 3).WithMessage(errorMessage: ResourceErrorMessage.NAME_MINIMUM_LENGTH)
             .MaximumLength(maximumLength: 200).WithMessage(errorMessage: ResourceErrorMessage.NAME_MAXIMUM_LENGTH);
