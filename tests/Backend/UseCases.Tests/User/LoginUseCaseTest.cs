b22f3e823e2ab7efd6d75ec1de4b4e02cc1b01e3
JonatasGdeC <jonatas.07022003@gmail.com>
2026-06-28 23:18:46 -0300

    Completando 'LoginUseCaseTest'

--- tests/Backend/UseCases.Tests/User/LoginUseCaseTest.cs
+++ tests/Backend/UseCases.Tests/User/LoginUseCaseTest.cs
@@ -6,12 +6,15 @@ using CommomTestsUtilies.Repositories.User;
 using CommomTestsUtilies.Requests.User;
 using CommomTestsUtilies.Token;
 using FluentAssertions;
+using FluentAssertions.Specialized;
 using Kanban.Application.UseCase.User.Login;
 using Kanban.Communication.Requests.User;
 using Kanban.Communication.Responses.User;
 using Kanban.Domain.Repositories.User;
 using Kanban.Domain.Security.Cryptography;
 using Kanban.Domain.Security.Tokens;
+using Kanban.Exception;
+using Kanban.Exception.ExceptionBase;
 
 namespace UseCases.Tests.User;
 using Kanban.Domain.Entities;
@@ -31,6 +34,33 @@ public class LoginUseCaseTest
         result.Should().NotBeNull();
         result.User.Name.Should().Be(expected: user.Name);
         result.Token.Should().NotBeNullOrWhiteSpace();
+    }
+    
+    [Fact]
+    public async Task Error_User_Not_Found()
+    {
+        User user = UserBuilder.Build();
+        LoginRequest request = LoginRequestBuilder.Build();
+        LoginUseCase useCase = CreateUseCase(user: user, password: request.Password);
+
+        Func<Task<LoginResponse>> act = async () => await useCase.Execute(request: request);
+
+        ExceptionAssertions<InvalidLoginException>? result = await act.Should().ThrowAsync<InvalidLoginException>();
+        result.Where(exceptionExpression: ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessage.INVALID_LOGIN));
+    }
+
+    [Fact]
+    public async Task Error_Password_Not_Match()
+    {
+        User user = UserBuilder.Build();
+        LoginRequest request = LoginRequestBuilder.Build();
+        request.Email = user.Email;
+        LoginUseCase useCase = CreateUseCase(user: user);
+    
+        Func<Task<LoginResponse>> act = async () => await useCase.Execute(request: request);
+    
+        ExceptionAssertions<InvalidLoginException>? result = await act.Should().ThrowAsync<InvalidLoginException>();
+        result.Where(exceptionExpression: ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessage.INVALID_LOGIN));
     }
     
     private LoginUseCase CreateUseCase(User user, string? password = null)
